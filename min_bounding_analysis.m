beeb3e24ee61dcc7fed8cd6a312f549add241058
FmDG <f.m.degraaf@qmul.ac.uk>
2022-05-06 12:08:30 +0100

    ADD PATHS
    Added paths to the min-bounding-triangle function for the initial analysis.

--- min_bounding_analysis.m
+++ min_bounding_analysis.m
@@ -1,16 +1,20 @@
 %% Import the dataset
 
 KM2_data = readtable("data/bounding_data/KM2.csv");
+modern_oceans = readtable("data/modern_oceans.csv");
 
 %% Print KM2 Data
 
-x = KM2_data.d18O;
-y = KM2_data.d13C;
+% Adds a path to the minimum bounding triangle function
+addpath("MinBoundSuite/MinBoundSuite")
 
-[tx,ty] = minboundtri(x,y);
-plot(x,y,'r+',tx,ty,'b-')
-text(KM2_data.d18O + 0.01, KM2_data.d13C + 0.01, KM2_data.Site, 'Fontsize', 10);
+[tx,ty] = minboundtri(modern_oceans.d18O,modern_oceans.d13C);
+plot(modern_oceans.d18O,modern_oceans.d13C,'r+',tx,ty,'b-')
+text(modern_oceans.d18O + 0.01, modern_oceans.d13C + 0.01, modern_oceans.Core, 'Fontsize', 10);
 
+%% Plot geospatial data
 
+scatter(modern_oceans.latitude, modern_oceans.longitude)
+text(modern_oceans.latitude + 0.1, modern_oceans.longitude + 0.1, modern_oceans.Core, 'Fontsize', 10);
 
 
