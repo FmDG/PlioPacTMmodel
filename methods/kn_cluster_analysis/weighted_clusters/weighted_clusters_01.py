4a1fc8b2b231654112190b7cb044e99a46158cbb
FmDG <f.m.degraaf@qmul.ac.uk>
2022-08-01 12:32:01 +0100

    WEIGHTED CLUSTERS

---
+++ methods/kn_cluster_analysis/weighted_clusters/weighted_clusters_01.py
@@ -0,0 +1,4 @@
+# I wonder if there is a way to categorize the data for weighting in the cluster analysis? Like sites that might be
+# influenced by boundary conditions (upwelling coastal) or marginal seas could be weighted less than the open ocean
+# sites?"
+
