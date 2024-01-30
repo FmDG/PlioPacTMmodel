8b4a37cf38111853da3e38d60ee21d37bdb8b43d
frisodegraaf <f.m.degraaf@qmul.ac.uk>
2024-01-30 15:11:12 +0000

    CLEARED UP SIMPLE MIXING MODEL This adds comments to the simple mixing model so that it makes sense rather than just being some science graphs on a screen. You're welcome future Friso.

--- methods/convergent_model/simple_mixing_model.py
+++ methods/convergent_model/simple_mixing_model.py
@@ -6,25 +6,41 @@ from pandas import read_csv
 
 from model_functions import simple_mixing_model
 
-chdir("../..")
-site_data = read_csv("data/prism_data.csv")
 
-section_name = "G20 - 2800 ka"
+def binary_mixing_model() -> None:
+    """
+    This function runs through all the PRISM data for the various time slices in the Pliocene. For each averaged value
+    of d18O and d13C it assumes that this is the Southern Sourced Water (SCW) endmember. It goes through all the other
+    sites' d18O and d13C values and tries to work out what the Northern Sourced Water (NCW) endmember would be if each
+    site had between 30 - 70% SCW%. If all the values for NCW composition converge at some point then one can assume
+    that this is a likely NCW endmember and that the site is a likely SCW endmember.
+    """
 
-subsection = site_data[site_data.TimePeriod == section_name]
-subsection = subsection.dropna()
+    chdir("../..")
+    site_data = read_csv("data/prism_data.csv")
 
-sites_list = ["odp1018", "odp1148", "odp1014", "odp1125", "odp1208", "odp1239", "dsdp594", "odp1143", "odp1241",
-              "odp806", "odp849", "dsdp593"]
+    section_name = "G20 - 2800 ka"
+
+    subsection = site_data[site_data.TimePeriod == section_name]
+    subsection = subsection.dropna()
+
+    sites_list = ["odp1018", "odp1148", "odp1014", "odp1125", "odp1208", "odp1239", "dsdp594", "odp1143", "odp1241",
+                  "odp806", "odp849", "dsdp593"]
 
 
-for site in sites_list:
-    scw = subsection[subsection.Site == site]
-    simple_model = simple_mixing_model(d18o=float(scw.d18O), d13c=float(scw.d13C), input_frame=subsection)
+    for site in sites_list:
+        scw = subsection[subsection.Site == site]
+        simple_model = simple_mixing_model(d18o=float(scw.d18O), d13c=float(scw.d13C), input_frame=subsection)
 
-    fig, ax = plt.subplots()
-    fig.suptitle(site)
-    sns.kdeplot(data=simple_model, x="NCW_d18O", y="NCW_d13C", ax=ax)
-    ax.scatter(float(scw.d18O), float(scw.d13C), marker="+", color="g")
-    print("Completed Site: {}".format(site))
-plt.show()
+        fig, ax = plt.subplots()
+        fig.suptitle(site)
+        sns.kdeplot(data=simple_model, x="NCW_d18O", y="NCW_d13C", ax=ax)
+        ax.scatter(float(scw.d18O), float(scw.d13C), marker="+", color="g")
+        print("Completed Site: {}".format(site))
+        ax.scatter(subsection.d18O, subsection.d13C, marker="+", color="b")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    binary_mixing_model()
